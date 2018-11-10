ffe48ef4dc46ba8c4d7c36310d92271bcf4c4cba
ymeramees <ymeramees@gmail.com>
2018-11-10 11:34:17 +0200

    Version changed to 0.4.0

--- src/version.h
+++ src/version.h
@@ -1,15 +1,15 @@
 ﻿#ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             0,3,2,0
-#define VER_FILEVERSION_STR         "0.3.2\0"
+#define VER_FILEVERSION             0,4,0,0
+#define VER_FILEVERSION_STR         "0.4.0\0"
 
-#define VER_PRODUCTVERSION          0,3,2,0
-#define VER_PRODUCTVERSION_STR      "0.3.2\0"
+#define VER_PRODUCTVERSION          0,4,0,0
+#define VER_PRODUCTVERSION_STR      "0.4.0\0"
 
 #define VER_COMPANYNAME_STR         "Ymeramees"
-#define VER_FILEDESCRIPTION_STR     "Protolehelugeja 0.3 Beta"
-#define VER_INTERNALNAME_STR        "Protolehelugeja 0.3 Beta"
+#define VER_FILEDESCRIPTION_STR     "Protolehelugeja 0.4 Beta"
+#define VER_INTERNALNAME_STR        "Protolehelugeja 0.4 Beta"
 #define VER_LEGALCOPYRIGHT_STR      "Copyright © 2018 Ymeramees"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
