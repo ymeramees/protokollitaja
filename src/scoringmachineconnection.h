af46d073724b198eb9793b6a87254ae604b76a95
ymeramees <ymeramees@gmail.com>
2019-03-31 15:43:41 +0300

    ReadfromRMIV and extractRMIVshot added with test

--- src/scoringmachineconnection.h
+++ src/scoringmachineconnection.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QTimer>
+#include <QLineF>
 #include <QtSerialPort/QSerialPort>
 #include <QtSerialPort/QSerialPortInfo>
 
@@ -12,10 +13,15 @@ class ScoringMachineConnection : public QObject
 {
     Q_OBJECT
 public:
-    explicit ScoringMachineConnection(QObject *parent = 0);
+    explicit ScoringMachineConnection(QObject *parent = nullptr);
     ~ScoringMachineConnection();
+    int noOfShotsPerTarget() const;
+    QString portName() const;
+    void setNoOfShotsPerTarget(int noOfShotsPerTarget);
+    void setPortName(const QString &portName);
     void setScoringMachineType(int machineType);
     void setTargetType(int targetType);
+    int targetType() const;
 
     enum ScoringMachineType {RMIII, RMIV};
     enum TargetType {AirRifle, AirPistol, SmallboreRifle};
@@ -23,6 +29,7 @@ public:
 private:
     bool m_connected;   // Shows if scoring machine is in connected stage
     bool m_machineChoiceInProgress;
+    int m_noOfShotsPerTarget;
     int m_scoringMachineType;
     int m_sendingStage;   // For RM-IV, showing in which stage sending currently is:
     // 0 - initial/ACK received (end), 1 -ENQ sent, 2 - STX received, 3 - text sent, 4 - text received, need to reply ACK
@@ -37,10 +44,11 @@ private:
 
     QTimer m_readTimer;   // Delay between reads from serial port
     QTimer m_sendTimer; // Delay before sending, so that RMIV has time to react
+    QTimer m_settingsTimer; // Delay before sending settings, so that machines have time to react
 
 signals:
     void dataSent(QByteArray data);
-    void shotRead(QString shotInfo);
+    void shotRead(Lask shot);
     void connectionStatusChanged(QString status);
 
 public slots:
@@ -49,6 +57,7 @@ public slots:
     void connectToMachine();
     Lask extractRMIIIShot(QString shotInfo);
     Lask extractRMIVShot(QString);
+    void sendSettings();
     void sendToMachine();
     void sendToMachine(QString);
 
