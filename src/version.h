77844763fff72bfc740680533af2a33779bcfc38
ymeramees <ymeramees@gmail.com>
2019-04-25 07:08:13 +0300

    Release 0.7.11

--- src/version.h
+++ src/version.h
@@ -2,11 +2,11 @@
 #define VERSION_H
 
 //Muudetud 19.01.2019 - peale mida oli reliis, seega on vaja uuendada
-#define VER_FILEVERSION             0,7,10,0
-#define VER_FILEVERSION_STR         "0.7.10\0"
+#define VER_FILEVERSION             0,7,11,0
+#define VER_FILEVERSION_STR         "0.7.11\0"
 
-#define VER_PRODUCTVERSION          0,7,10,0
-#define VER_PRODUCTVERSION_STR      "0.7.10\0"
+#define VER_PRODUCTVERSION          0,7,11,0
+#define VER_PRODUCTVERSION_STR      "0.7.11\0"
 
 #define VER_COMPANYNAME_STR         "Ymeramees"
 #define VER_FILEDESCRIPTION_STR     "Protokollitaja 0.7 Beta"
