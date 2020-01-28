2b580fdfefce7af9660dceac0c92797a9862ddd1
ymeramees <ymeramees@gmail.com>
2020-01-28 20:41:51 +0200

    Release 0.7.15

--- main/version.h
+++ main/version.h
@@ -1,23 +1,23 @@
 ﻿#ifndef VERSION_H
 #define VERSION_H
 
-// Changed 12.07.2019 - after which there has been a release, so need to update
-#define VER_FILEVERSION             0,7,14,0
-#define VER_FILEVERSION_STR         "0.7.14\0"
+// Changed 28.01.2020 - after which there has been a release, so need to update
+#define VER_FILEVERSION             0,7,15,0
+#define VER_FILEVERSION_STR         "0.7.15\0"
 
-#define VER_PRODUCTVERSION          0,7,14,0
-#define VER_PRODUCTVERSION_STR      "0.7.14\0"
+#define VER_PRODUCTVERSION          0,7,15,0
+#define VER_PRODUCTVERSION_STR      "0.7.15\0"
 
 #define VER_COMPANYNAME_STR         "Ymeramees"
 #define VER_FILEDESCRIPTION_STR     "Protokollitaja 0.7 Beta"
 #define VER_INTERNALNAME_STR        "Protokollitaja 0.7 Beta"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2019 Ymeramees"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2020 Ymeramees"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "Protokollitaja.exe"
 #define VER_PRODUCTNAME_STR         "Protokollitaja"
 
-#define VER_AASTA		    "2019"
+#define VER_AASTA		    "2020"
 
 //#define VER_COMPANYDOMAIN_STR       "example.org"
 
