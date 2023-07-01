bc1ae754548c4f5816eb14bdcb6d55b6eea7209b
ymeramees <ymeramees@gmail.com>
2023-07-01 08:53:16 +0300

    Pump version, try withouth openssl libs

--- common/src/version.h
+++ common/src/version.h
@@ -1,12 +1,12 @@
 ﻿#ifndef VERSION_H
 #define VERSION_H
 
-// Changed 27.05.2023 - after which there has not been a release, so no need to update
-#define VER_FILEVERSION             0,8,3,0
-#define VER_FILEVERSION_STR         "0.8.3\0"
+// Changed 01.07.2023 - after which there has not been a release, so no need to update
+#define VER_FILEVERSION             0,9,0,0
+#define VER_FILEVERSION_STR         "0.9.0\0"
 
-#define VER_PRODUCTVERSION          0,8,3,0
-#define VER_PRODUCTVERSION_STR      "0.8.3\0"
+#define VER_PRODUCTVERSION          0,9,0,0
+#define VER_PRODUCTVERSION_STR      "0.9.0\0"
 
 #define VER_COMPANYNAME_STR         "Ymeramees"
 #define VER_LEGALCOPYRIGHT_STR      "Copyright © 2023 Ymeramees"
